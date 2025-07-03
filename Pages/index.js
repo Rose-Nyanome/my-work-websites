e62b8b34d5e522f71a9a84aab38e4f39c8675b6b
Rose-Nyanome <rnyanome@gmail.com>
2025-07-02 23:13:58 -0700

    Add files via upload

---
+++ Pages/index.js
@@ -0,0 +1,10 @@
+latter ='please come home';
+console.log( latter);
+// This my first java script
+let name = 'killer';
+console.log(name);
+//this is numbers 
+age= 40
+console.log(age);
+//This is math
+
